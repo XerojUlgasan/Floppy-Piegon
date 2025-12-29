39ebbb67174104601e3c3c87296d64c10075d7c5
Xeroj Nilles Ulgasan <xerojulgasan@gmail.com>
2025-12-29 17:40:31 +0800

    DONE!
    Added death when out of screen.

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -1,24 +1,42 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public LogicScript logic;
+    public Camera cam;
     public float flapStrength;
     public float birdSpeed;
+    public bool isAlive { get; set; } = true;
+
+    private LogicScript logic;
+    private float top, down, left, right;
+    private float screenRation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("logic").GetComponent<LogicScript>();
+        top = cam.transform.position.y + cam.orthographicSize;
+        down = cam.transform.position.y - cam.orthographicSize;
+        left = cam.transform.position.x - cam.orthographicSize * cam.aspect;
+        right = cam.transform.position.x + cam.orthographicSize * cam.aspect;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isAlive)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, flapStrength);
+        }
+
+        Vector3 self = gameObject.transform.position;
+
+        if (self.y > top || self.y < down ||
+            self.x < left || self.x > right)
+        {
+            logic.resetScore();
         }
     }   
 }
--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
     public UnityEngine.UI.Text scoreText;
+    public GameObject gameOverScreen;
+    public BirdScript bird;
     private int score = 0;
+
+    void Start()
+    {
+        bird = GameObject.FindGameObjectWithTag("Player").GetComponent<BirdScript>();
+    }
 
     [ContextMenu("Increase Score")]
     public void addScore()
@@ -17,5 +24,14 @@ public class LogicScript : MonoBehaviour
     {
         score = 0;
         scoreText.text = score.ToString();
+        gameOverScreen.SetActive(true);
+        bird.isAlive = false;
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameOverScreen.SetActive(false);
+        bird.isAlive = true;
     }
 }
